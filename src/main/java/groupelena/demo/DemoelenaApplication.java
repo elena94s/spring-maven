9b728342d5810ac635c406cf79e351d1f14f6d79
Elena Smail <smail.elena94@gmail.com>
2026-01-15 09:44:38 +0100

    booya

---
+++ src/main/java/groupelena/demo/DemoelenaApplication.java
@@ -0,0 +1,13 @@
+package groupelena.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoelenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoelenaApplication.class, args);
+	}
+
+}
